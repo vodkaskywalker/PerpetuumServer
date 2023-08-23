158d2d8e7be02f5dc9ad329b9f0f1603cfcc4b22
Viktor Dolzhukov <viktor.dolzhukov@intravision.ru>
2023-08-24 01:00:58 +0300

    - Re-adding files with correct encoding

---
+++ src/Perpetuum/Threading/LockFree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace Perpetuum.Threading
+{
+    public static class LockFree
+    {
+        public static void Update(ref uint destination, Func<uint, uint> updater)
+        {
+            var spinWait = new SpinWait();
+
+            unsafe
+            {
+                fixed (uint* p = &destination)
+                {
+                    while (true)
+                    {
+                        var original = *p;
+                        var value = updater(original);
+
+                        if (Interlocked.CompareExchange(ref *(int*)p, (int)value, (int)original) == original)
+                            return;
+
+                        spinWait.SpinOnce();
+                    }
+                }
+            }
+        }
+
+
+        public static void Swap<T>(ref T a, ref T b) where T : class
+        {
+            Interlocked.Exchange(ref a, Interlocked.Exchange(ref b, a));
+        }
+    }
+}
