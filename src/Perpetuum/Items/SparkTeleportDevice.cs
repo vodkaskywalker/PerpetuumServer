c5e38dcff4f67a46b0d79a16f26c598cd040bbb7
Viktor Dolzhukov <viktor.dolzhukov@intravision.ru>
2023-09-04 17:33:17 +0300

    ﻿Spark Teleport Device
    - Renaming Spark Teleport Token into Spark Teleport Device

---
+++ src/Perpetuum/Items/SparkTeleportDevice.cs
@@ -0,0 +1,25 @@
+﻿using Perpetuum.Accounting;
+using Perpetuum.Accounting.Characters;
+using Perpetuum.Common.Loggers.Transaction;
+using Perpetuum.Data;
+using Perpetuum.Host.Requests;
+using Perpetuum.Players;
+using Perpetuum.Robots;
+using Perpetuum.Services.Sparks.Teleports;
+using System;
+using System.Collections.Generic;
+using System.Transactions;
+
+namespace Perpetuum.Items
+{
+    public class SparkTeleportToken : Item
+    {
+        public int BaseId
+        {
+            get
+            {
+                return ED.Options.GetOption<int>("baseId");
+            }
+        }
+    }
+}
